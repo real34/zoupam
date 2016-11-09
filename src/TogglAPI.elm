5b83c3c5f2977586d44a4d805f648313f6cc31f9
Pierre Martin <pierre@occitech.fr>
2016-11-09 23:01:44 +0100

    Refactoring: extract types for querying Toggl

--- src/TogglAPI.elm
+++ src/TogglAPI.elm
@@ -4,6 +4,7 @@ import Json.Decode as Json exposing ((:=))
 import Json.Decode.Extra exposing ((|:))
 import Http
 import Task
+import String
 import Base64
 
 
@@ -14,6 +15,22 @@ type alias TimeEntry =
     , duration : Int
     }
 
+type alias Context =
+    { workspaceId: Int
+    , userAgent: String
+    }
+
+-- Source: https://github.com/toggl/toggl_api_docs/blob/master/reports.md#request-parameters
+-- NB! Maximum date span (until - since) is one year.
+type OnOff = On | Off
+type alias RequestParameters =
+    { projectIds: Maybe (List Int)
+    , clientIds: Maybe (List Int)
+    , since: Maybe String
+    , until: Maybe String
+    , rounding: Maybe OnOff
+    }
+
 durationInMinutes : Int -> Float
 durationInMinutes duration =
   toFloat duration / 60 / 60 / 1000
@@ -22,20 +39,40 @@ baseUrl : String
 baseUrl =
     "https://toggl.com/reports/api/v2"
 
+buildContextParams : Context -> List (String, String)
+buildContextParams context =
+    [ ( "user_agent", context.userAgent )
+    , ( "workspace_id", toString context.workspaceId )
+    ]
+
+buildIdsListParam : Maybe (List Int) -> String
+buildIdsListParam ids =
+    (Maybe.withDefault [] ids) |> List.map toString |> List.intersperse "," |> String.concat
+
+buildRequestParams : RequestParameters -> List (String, String)
+buildRequestParams request =
+    [ ( "project_ids", buildIdsListParam request.projectIds )
+    , ( "client_ids", buildIdsListParam request.clientIds )
+    , ( "since", Maybe.withDefault "" request.since )
+    , ( "until", Maybe.withDefault "" request.until )
+    , ( "rounding", (Maybe.withDefault Off request.rounding) |> toString )
+    ]
 
 getDetails : String -> (Http.Error -> msg) -> (List TimeEntry -> msg) -> Cmd msg
 getDetails key errorMsg msg =
     let
+        context = Context 127309 "contact@occitech.fr"
+        params = RequestParameters
+            (Just [22791424])
+            Nothing
+            (Just "2016-01-01")
+            Nothing
+            (Just Off)
+
         request =
             { verb = "GET"
             , headers = [ ( "Authorization", "Basic " ++ (Result.withDefault "bob" (Base64.encode (key ++ ":api_token"))) ) ]
-            , url =
-                Http.url (baseUrl ++ "/details")
-                    [ ( "user_agent", "adrien@occitech.fr" )
-                    , ( "workspace_id", "127309" )
-                    , ( "project_ids", "22791424" )
-                    , ( "since", "2016-01-01" )
-                    ]
+            , url = Http.url (baseUrl ++ "/details") (( buildContextParams context ) ++ ( buildRequestParams params ))
             , body = Http.empty
             }
     in
